742cd696ba58bb99f1f79c9e7b402d2950fc8e9c
Enrique Melero <emelero.dev@pm.me>
2023-06-23 11:11:24 +0200

    Initial scaffolding and config override.

---
+++ config/readme.txt
@@ -0,0 +1,4 @@
+Place config files on config/game/ and config/login/ folders and run
+scripts/config.sh to override configuration values on a target release.
+
+Ex: ./config.sh config/game release/{RELEASE_ID}/game/config
---
+++ scripts/config.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+default_source_dir="../config/game"
+default_target_dir="../current/game/config"
+
+source_dir="${1:-$default_source_dir}"
+target_dir="${2:-$default_target_dir}"
+
+echo "> Applying configuration overrides..."
+echo ""
+
+trim_ws() {
+  local input="$1"
+  local trimmed=$(echo "$input" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+  echo "$trimmed"
+}
+
+for source_config in "$source_dir"/*; do
+
+  source_file=$(basename "$source_config")
+  target_config="$target_dir/$source_file"
+
+  echo "$source_file"
+  echo "---------------------------------"
+
+  if [[ -f "$target_config" ]]; then
+    changes=0
+    while IFS='=' read -r field value; do
+      if [[ $field =~ ^\s*# ]] || [[ -z $field ]]; then
+        continue
+      fi
+
+      field=$(trim_ws $field)
+      value=$(trim_ws $value)
+
+      found=$(grep -E "^[^#]*$field\s*=" $target_config)
+      if [[ ! -n $found ]]; then
+        echo "$field missing on target config"
+        continue
+      fi
+
+      sed -i "s/${field}\s*=\s*.*/${field} = ${value}/" "$target_config"
+
+      changes=$((changes + 1))
+      echo "$field >> $value"
+    done < "$source_config"
+
+    if [ $changes -eq 0 ]; then
+      echo "No changes found on $source_config."
+    else
+      echo "Modified $target_config."
+    fi
+  else
+    echo "Target config file $target_config not found."
+  fi
+  echo ""
+done
+
+echo "> END"
